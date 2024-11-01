18ef57051bcd64e8190b06211b1e6072c5f3b9f6
musyaffa-iman <musyaffa.iman@gmail.com>
2024-11-01 23:14:32 +0700

    CS8 - Submitted Musyaffa Iman Supriadi

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player PlayerInstance { get; private set; }
+    public PlayerMovement playerMovement;
+    public Animator animator;
+
+    private void Awake()
+    {
+        // Check apakah sebuah PlayerInstance sudah ada
+        if (PlayerInstance == null)
+        {
+            PlayerInstance = this; // Set object sebagai Singleton PlayerInstance
+            DontDestroyOnLoad(gameObject); // Mencegah destruction on scene load
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy PlayerInstance duplikat
+        }
+    }
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        animator =  GameObject.Find("Engine/EngineEffect").GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        playerMovement.Move();
+    }
+
+    void LateUpdate()
+    {
+        animator.SetBool("IsMoving", playerMovement.IsMoving());
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Vector2 maxSpeed;
+    [SerializeField] Vector2 timeToFullSpeed;
+    [SerializeField] Vector2 timeToStop;
+    [SerializeField] Vector2 stopClamp;
+
+    Vector2 moveDirection;
+    Vector2 moveVelocity;
+    Vector2 moveFriction;
+    Vector2 stopFriction;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveVelocity = 2 * maxSpeed / timeToFullSpeed;
+        moveFriction = -2 * maxSpeed/(timeToFullSpeed * timeToFullSpeed);
+        stopFriction = -2 * maxSpeed/(timeToStop * timeToStop);
+    }
+
+    public void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector2 currentVelocity = rb.velocity;
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        
+        if (moveDirection != Vector2.zero)
+        {
+            currentVelocity.x += moveDirection.x * moveVelocity.x * Time.deltaTime;
+            currentVelocity.y += moveDirection.y * moveVelocity.y * Time.deltaTime;
+        }
+        else
+        {
+            if (Mathf.Abs(currentVelocity.x) < stopClamp.x)
+            {
+                currentVelocity.x = 0;
+            }
+            else
+            {   
+                if (currentVelocity.x > 0)
+                    currentVelocity.x += GetFriction().x * Time.deltaTime;
+                else if (currentVelocity.x < 0)
+                    currentVelocity.x -= GetFriction().x * Time.deltaTime;
+            }
+
+            if (Mathf.Abs(currentVelocity.y) < stopClamp.y)
+            {
+                currentVelocity.y = 0;
+            }
+            else
+            {
+                if (currentVelocity.y > 0)
+                    currentVelocity.y += GetFriction().y * Time.deltaTime;
+                else if (currentVelocity.y < 0)
+                    currentVelocity.y -= GetFriction().y * Time.deltaTime;
+            }
+        }
+
+        currentVelocity.x = Mathf.Clamp(currentVelocity.x, -maxSpeed.x, maxSpeed.x);
+        currentVelocity.y = Mathf.Clamp(currentVelocity.y, -maxSpeed.y, maxSpeed.y);
+
+        if (currentVelocity.magnitude > maxSpeed.magnitude)
+        {
+            currentVelocity = currentVelocity.normalized * maxSpeed.magnitude;
+        }
+
+        rb.velocity = currentVelocity;
+    }
+
+    public Vector2 GetFriction()
+    {
+        return rb.velocity != Vector2.zero ? moveFriction : stopFriction;
+    }
+
+    public void MoveBound()
+    {
+        
+    }
+
+    public bool IsMoving()
+    {
+        return rb.velocity.magnitude > 0;
+    }
+}
